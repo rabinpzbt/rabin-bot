581a41294e05a16ee6f4fd060af18a37616027e1
Rabin Bote <rabinbot@gmail.com>
2023-04-07 17:58:40 +0800

    rabinbot title

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,5 +1,5 @@
 module.exports = {
-	"title": "Rabin Bote",
+	"title": "rabinbot",
 	"url": "https://example.com/",
 	"language": "en",
 	"description": "I am writing about my experiences as a naval navel-gazer.",
